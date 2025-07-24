f73eb78ab7b30474ceabc2cc268e0adb2cb668cd
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-24 19:35:47 +0700

    Digunakan untuk menjalankan perintah yang tidak membutuhkan input.

---
+++ funstions.kt
@@ -0,0 +1,13 @@
+package com.example.test
+
+fun withoutParameter() {
+    println("=== withoutParameter ===")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("=== withParameter ===")
+    println("Hello, $name!")
+}
+
