07f44b0f04107ae6765cdeff3bd9b4b628f66a5a
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-24 19:36:26 +0700

    memudahkan pembacaan dan fleksibilitas saat memanggil fungsi.

--- funstions.kt
+++ funstions.kt
@@ -11,3 +11,29 @@ fun withParameter(name: String) {
     println("Hello, $name!")
 }
 
+//named argument
+// /named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("=== withNamedArgument ===")
+    println("Hello, $name! You are $age years old.")
+}
+
+//default parameter value
+// /default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("=== withDefaultParameter ===")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int) : Int {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
