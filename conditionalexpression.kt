d0b99816b6017c85867282c45f69df5c42191fdd
Inggar Kinanthi <inggarkinanthi@example.com>
2025-07-24 19:30:24 +0700

    penjelasan lengkap untuk fungsi Perulangan() dalam Kotlin

--- conditionalexpression.kt
+++ conditionalexpression.kt
@@ -36,3 +36,36 @@ fun ConditionalStatement() {
         println("Invalid Number")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    // For (i in 1..5) {
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
+}
